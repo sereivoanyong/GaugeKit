99d75bdb17cd47983f6a7a62340f6704092322b7
Sereivoan Yong <sereivoanyong@gmail.com>
2023-08-21 01:24:14 +0700

    Fix `ringGradientLayer` setup

--- GaugeKit/GaugeCircle.swift
+++ GaugeKit/GaugeCircle.swift
@@ -31,6 +31,23 @@ extension Gauge: GaugeCircle {
             ringLayer.frame = layer.bounds
             gaugeLayer.addSublayer(ringLayer)
         }
+
+        if ringGradientLayer == nil {
+            ringGradientLayer = CAGradientLayer()
+            if isCircle && (layer.bounds.width < layer.bounds.height) {
+                let adjust: CGFloat = (layer.bounds.height - layer.bounds.width) / 2 / layer.bounds.height
+                ringGradientLayer.startPoint = CGPoint(x: 0.5, y: 1 - adjust)
+                ringGradientLayer.endPoint = CGPoint(x: 0.5, y: adjust)
+            } else {
+                ringGradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
+                ringGradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
+            }
+            ringGradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+            ringGradientLayer.frame = layer.bounds
+            ringGradientLayer.mask = ringLayer
+            gaugeLayer.addSublayer(ringGradientLayer)
+        }
+
         gaugeLayer.frame = layer.bounds
         gaugeLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         // Rotate it in 90° CCW to make start position from the top
